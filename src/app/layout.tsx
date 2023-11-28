8ec4b43dc64fa722e01cfcfbdbc75f1af2d89521
0xernesto <78889960+0xernesto@users.noreply.github.com>
2023-11-28 19:04:55 +0800

    chore: update next.js, add card image, custom 404 page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,104 @@
 import "../styles/globals.css";
 
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import React from "react";
 
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
+export const viewport: Viewport = {
+	themeColor: "#000000", // dark-600
+};
+
 export const metadata: Metadata = {
+	metadataBase: new URL("https://chainrule.io"),
 	title: "Chain Rule",
 	description:
 		"We partner with founders to build and ship secure, robust DeFi products across multiple blockchains.",
+	keywords: [
+		"Open-Source",
+		"Code",
+		"Math",
+		"Quant",
+		"Quantitative",
+		"Analysis",
+		"Testing",
+		"Chain Rule",
+		"xSafe",
+		"DeFi",
+		"Crypto",
+		"Protocol",
+		"Distributed Systems",
+		"Security",
+		"Smart Contracts",
+		"EVM",
+		"Deploy",
+		"Cross-Chain",
+		"Multi-Chain",
+		"Ethereum",
+	],
+	icons: {
+		icon: [
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "32x32",
+				url: "/favicon-32x32.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "16x16",
+				url: "/favicon-16x16.png",
+			},
+			{
+				rel: "apple-touch-icon",
+				sizes: "180x180",
+				url: "/apple-touch-icon.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "192x192",
+				url: "/android-chrome-192x192.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "512x512",
+				url: "/android-chrome-512x512.png",
+			},
+			{
+				rel: "mask-icon",
+				url: "/safari-pinned-tab.svg",
+				color: "#4f585c", // dark-200
+			},
+		],
+	},
+	openGraph: {
+		title: "Chain Rule",
+		description:
+			"We partner with founders to build and ship secure, robust DeFi products across multiple blockchains.",
+		url: "https://chainrule.io",
+		type: "website",
+		images: [
+			{
+				url: "/card-image.png",
+			},
+		],
+	},
+	twitter: {
+		title: "Chain Rule",
+		description:
+			"We partner with founders to build and ship secure, robust DeFi products across multiple blockchains.",
+		card: "summary_large_image",
+		images: [
+			{
+				url: "/card-image.png",
+			},
+		],
+		site: "@chainrule_io",
+	},
 };
 
 export default function RootLayout({
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,23 +3,31 @@ import { FaGithub, FaXTwitter } from "react-icons/fa6";
 
 function Footer() {
 	return (
-		<footer className="flex w-full items-center justify-center p-4">
-			<a
-				className="flex h-12 w-12 flex-shrink-0 items-center justify-center hover:text-primary-200"
-				href={process.env.GITHUB_URL}
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<FaGithub size="28px" />
-			</a>
-			<a
-				className="flex h-12 w-12 flex-shrink-0 items-center justify-center hover:text-primary-200"
-				href={process.env.TWITTER_URL}
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<FaXTwitter size="28px" />
-			</a>
+		<footer className="flex w-full flex-col items-center justify-center p-4">
+			<div className="flex w-full max-w-screen-xl items-center justify-center">
+				<a
+					className="flex h-12 w-12 flex-shrink-0 items-center justify-center hover:text-primary-200"
+					href={process.env.GITHUB_URL}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<FaGithub size="28px" />
+				</a>
+				<a
+					className="flex h-12 w-12 flex-shrink-0 items-center justify-center hover:text-primary-200"
+					href={process.env.TWITTER_URL}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<FaXTwitter size="28px" />
+				</a>
+			</div>
+			<div className="mt-4 flex flex-col py-2">
+				<span className="text-center text-sm">
+					© 2023 Chain Rule, LLC
+				</span>
+				<span className="text-center text-sm">All rights reserved</span>
+			</div>
 		</footer>
 	);
 }
